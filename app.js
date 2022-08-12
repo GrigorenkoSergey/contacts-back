9158e879b7d4f9bfe56b2fab84fc4329ffcdbfc0
Sergey Grigorenko <sergey.grigorenko@metalamp.io>
2022-08-12 14:49:24 +0300

    added build

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+app.use(express.static('./public'));
 app.use(express.json());
 
+app.use('/contacts', (req, res) => res.redirect('/'));
 app.use('/api/v1', mainRouter);
 
 app.use(notFoundMiddleware);
